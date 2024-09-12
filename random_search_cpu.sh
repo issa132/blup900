51e53010ffe7b910204219e0160a344fe9f8182a
issa132 <issa132@hotmail.com>
2024-09-12 17:14:00 -0400

    Update random_search_cpu.sh

--- random_search_cpu.sh
+++ random_search_cpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --account=def-banire
 #SBATCH --cpus-per-task=16   # number of cores
-#SBATCH --mem=4G            # memory for the entire job across all cores (4GB)
+#SBATCH --mem=12G            # memory for the entire job across all cores (4GB)
 #SBATCH --time=7-00:00      # time (DD-HH:MM)
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
